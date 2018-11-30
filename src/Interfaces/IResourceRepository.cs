5ebe8aef3eecf1a3c3a850c43cc08b50d97637fc
Ronan Doherty <ronan.odochartaigh@gmail.com>
2018-11-30 17:48:18 +0000

    Migrate to async methods

--- src/Interfaces/IResourceRepository.cs
+++ src/Interfaces/IResourceRepository.cs
@@ -1,13 +1,14 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Schematic.Core
 {
     public interface IResourceRepository<T, TResourceFilter>
     {
-        int Create(T resource, int userID);
-        T Read(int id);
-        bool Update(T resource, int userID);
-        bool Delete(int id, int userID);
-        List<T> List(TResourceFilter filter);
+        Task<int> Create(T resource, int userID);
+        Task<T> Read(int id);
+        Task<int> Update(T resource, int userID);
+        Task<int> Delete(int id, int userID);
+        Task<List<T>> List(TResourceFilter filter);
     }
 }
