51614ef2b697439c099d15c82e7d1e8db823fcb2
Ronan Doherty <ronan.odochartaigh@gmail.com>
2018-12-04 15:20:18 +0000

    Added support for custom-defined generic controller names

--- src/Attributes/SchematicResourceAttribute.cs
+++ src/Attributes/SchematicResourceAttribute.cs
@@ -5,11 +5,13 @@ namespace Schematic.Core
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class SchematicResourceAttribute : Attribute
     {
-        public SchematicResourceAttribute(string route = "")
+        public SchematicResourceAttribute(string controllerName = "", string route = "")
         {
+            ControllerName = controllerName;
             Route = route;
         }
             
+        public string ControllerName { get; set; }
         public string Route { get; set; }
     }
 }
