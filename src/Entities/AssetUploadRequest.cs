a7af5ae4d174c4e9e8c8976d0191e0c1053f759d
Ronan Doherty <ronan.odochartaigh@gmail.com>
2018-12-07 18:03:52 +0000

    Updated interface for IAssetStorageService

---
+++ src/Entities/AssetUploadRequest.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Http;
+
+namespace Schematic.Core
+{
+    public class AssetUploadRequest
+    {
+        public string ContainerName { get; set; }
+        
+        public IFormFile File { get; set; }
+
+        public string FilePath { get; set; }
+    }
+}
