08f51e3337ea5ebef8c24874ff85bd14eb1b0a38
Ronan Doherty <ronan.odochartaigh@gmail.com>
2018-12-09 17:01:44 +0000

    Allow anonymous resource access when no AuthorizeAttribute is present
    "Anonymous" in the sense that the user does not need a particular role or policy permission to access the resource; they will still be affected by standard MVC authorisation policy

--- src/ResourceAuthorization.cs
+++ src/ResourceAuthorization.cs
@@ -9,6 +9,13 @@ namespace Schematic.Core
     {
         public static bool IsAuthorized(this ClaimsPrincipal user, Type type)
         {
+            // If the type has no SchematicAuthorizeAttribute the user is authorised
+            if (!type.IsDefined(typeof(SchematicAuthorizeAttribute), false))
+            {
+                return true;
+            }
+
+            // Otherwise, get attribute value and test against user identity
             string requiredRole = type.GetAttributeValue((SchematicAuthorizeAttribute a) => a.Role);
 
             ClaimsIdentity ClaimsIdentity = user.Identity as ClaimsIdentity;
